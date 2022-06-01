b9f8f0fe12b41956d652fe5f29ae52a5e6d29614
lineJeong <103614912+lineJeong@users.noreply.github.com>
2022-06-01 21:28:30 +0900

    Time: 71 ms (72.01%), Space: 42.3 MB (74.03%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.js
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.js
@@ -3,10 +3,11 @@
  * @return {number[]}
  */
 var runningSum = function(nums) {
-    let result = []
-    result[0] = nums[0]
+    let result = [nums[0]];
     for (i = 1; i < nums.length; i++) {
         result.push(result[i - 1] + nums[i])
     }  
-    return result
+    return result;
 };
+
+
