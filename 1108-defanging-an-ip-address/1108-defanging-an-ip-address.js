ff87f4328cfecbbab507be678d6dff9813ad4f0d
lineJeong <103614912+lineJeong@users.noreply.github.com>
2022-06-11 02:06:22 +0900

    Time: 107 ms (8.41%), Space: 41.9 MB (45.16%) - LeetHub

---
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} address
+ * @return {string}
+ */
+var defangIPaddr = function(address) {
+    let result = "";
+    for(let i=0; i<address.length; i++) {
+        if(address[i] === '.') result = result + '[.]';
+        else result = result + address[i];
+    }
+    return result;
+};
