be5a7e8588a045167f2d84491335bf1f9772c5a1
lineJeong <103614912+lineJeong@users.noreply.github.com>
2022-06-15 22:55:03 +0900

    Time: 98 ms (23.33%), Space: 42.4 MB (36.22%) - LeetHub

---
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[][]} accounts
+ * @return {number}
+ */
+var maximumWealth = function(accounts) {
+    let max = 0;
+    let sum = 0;
+    
+    for(let i=0; i<accounts.length; i++) {
+        for(let j=0; j<accounts[i].length; j++) {
+            sum = sum + accounts[i][j]
+        }
+        if(max < sum) max = sum;
+        sum = 0;
+    }
+    return max;
+};
